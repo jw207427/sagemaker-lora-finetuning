187461d7224cdeb89f5717f77d353a58abceb4c5
James Wu <jingswu@amazon.com>
2023-08-05 21:46:32 +0000

    model registry and deployment

--- falcon/deepspeed/model.py
+++ falcon/deepspeed/model.py
@@ -5,17 +5,39 @@ from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 from typing import Any, Dict, Tuple
 import deepspeed
 import warnings
+import tarfile
 
 predictor = None
+model_tar = "model.tar.gz"
 
 
 def get_model(properties):
+    
+    # location on the hosting instance where the model checkpoints are downloaded (from the s3url)
     model_name = properties["model_id"]
+    print(f"List model directory: {os.listdir(model_name)}")
+    
+    tar_path = os.path.join(model_name, model_tar)
+    print(f"extract file: {tar_path}")
+    # Open the tar.gz file
+    with tarfile.open(tar_path, "r:gz") as tar:
+        # Extract all contents of the tar.gz file
+        tar.extractall(model_name)
+    
+    print("Extraction complete.")
+    print(f"List model directory again: {os.listdir(model_name)}")
+
+    
     local_rank = int(os.getenv("LOCAL_RANK", "0"))
+    
+    print(f"Loading model from {model_name}")
     model = AutoModelForCausalLM.from_pretrained(
-        model_name, low_cpu_mem_usage=True, trust_remote_code=True, torch_dtype=torch.bfloat16
+        model_name, low_cpu_mem_usage=True,trust_remote_code=True,
+torch_dtype=torch.bfloat16
     )
     model = deepspeed.init_inference(model, mp_size=properties["tensor_parallel_degree"])
+    
+    print(f"Loading tokenizer from {model_name}")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     generator = pipeline(
         task="text-generation", model=model, tokenizer=tokenizer, device=local_rank
